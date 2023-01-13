0dd3a9ecb2461074f8c340c95920a63a8308b3a2
dipayankundu25 <dipayankundu25@gmail.com>
2023-01-13 19:31:09 +0530

    Update

--- Employee_Crud/src/app/app.component.spec.ts
+++ Employee_Crud/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular_Project'`, () => {
+  it(`should have as title 'Employee_Crud'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular_Project');
+    expect(app.title).toEqual('Employee_Crud');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Angular_Project app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Employee_Crud app is running!');
   });
 });
